2b8bb0e60f737afcbba3b952e1e73a749f36109e
suhar <suhartx@opendeusto.es>
2023-03-27 21:03:32 +0200

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -8,8 +8,8 @@ variable "ec2_key_name" {
  default = "id_rsa"
 }
 variable "ec2_public_key" {
- default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFYPok13wdQ/01DMo/tLHJge65W6ivvjn7b6N9GmYJp6bApuguThlDxOyhdE147tfXPBqKIcYGBTok03ky3vr8XphvCESGwGmZh60y3/jF5ViC5/n0H8zrRagqKARzJ8Gl7ngxI+1J8STeM156q+nuz/cj5LT44r/46EcDOEfXP5+Zh2YnYMtwKLHSUn/7pmF9uNoTBK5IRtOsGPZlEGbIsWTerRsAqnbomGgoYzw8c1xfrHzHYO+sHJTrFhKwlrqW4GjHl/w3lPVHf5CC6Skas8MaR43eNzEFfxm4OMsWME0DpA6JHJmcev8aXSkpI+zeTdY560jyaGVL6ZEOnI86MP52/9vjKGYBMJ2OZWI1EJotZUVKm217jmBAJXuK5PMeis1Ra+OMH9VwU6ewdD6pov8a7Oh7r15vJQi33MO/+zEQ/M9lJIyhSCBOHKMlmMHdDRUocJ3GtHP18ObdSS/qxbCkXZa6HnE7g99rUcWNGJdMqmH0oRiECFU7wV8a92c= bosonit\\suhar.txabarri@EM2021002676"
+ default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDF9tLaYw1lDAsRbJaVVxJPFGfZvvpDDXL1qZeg3t8zdY2zlfQhVtn3LHHw2Ap3UWS9qBavXkzZYAA4AAn5ZoL5SzEkTbl8At9pvwmu9jVYhLzZQU1jB0AovpWdjAktnAML4CknnEZY1HRvEkRW4D7CSB86Gxrd7aHC/pTxi7RH4Tzm03s1ZiThXPkS/RKel5SoY7I8Rmx1XKwWW83CxDg26T/mPk7xSq3k/+If4Jj6ucSnLxTc/ryBNKypGHJjZplPr4ZSfP/AuPGnO1lxUHZglozfnJf/Kwp6R0f7Lyy4A0eAiX0pdm/2SlK9CzG76O1YxbCxb4AYPFMY1zL4bROjGmOKZDkiJVcfWkm1qel/TMUV4GbadqNQxNl2z0KokTXhI1z5MgkHb2sVx7fAPgAUCSw8AS4PNKZg8T1p/ROrc3qJMhf8nCurNFwjRWSKDWWMb6uid+MMfSckq9a14y1oEsOBieJ+2XWZQbLvFd7eAGx3GB8gimwn20a+nwG62/E= maita@LAPTOP-BPQS4KH4"
 }
 variable "ec2_private_key_file_path" {
- default = "<C:/Users/suhar.txabarri/Desktop/Suhar/TFG/Terraform/ssh/id_rsa_suhar"
+ default = "<C:/Users/maita/.ssh/id_rsa"
 }
